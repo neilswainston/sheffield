9d05e676eac631df31a2f5f497658be5dbe5116c
neilswainston <neil.swainston@manchester.ac.uk>
2018-06-06 13:42:25 +0100

    Updated chapter 4 analysis.

--- sheffield/chapter_4.py
+++ sheffield/chapter_4.py
@@ -4,23 +4,60 @@ Created on 6 Jun 2018
 @author: neilswainston
 '''
 import sys
+
 import cobra
+from cobra.flux_analysis import flux_variability_analysis
 
 
 def prelim_analysis(filename):
     model = _get_model(filename)
 
     # Fix glucose uptake to 1 and maximise:
-    _fix_glucose_maximise(model, 1)
-    _fix_glucose_maximise(model, 10)
+    _fix_glucose_maximise(model, 1, 'Max biomass, glc_uptake = 1')
+    _fix_glucose_maximise(model, 10, 'Max biomass, glc_uptake = 10')
 
     # Add mAb production to objective function and maximise:
     model.reactions.BIO029.objective_coefficient = 1
-    _fix_glucose_maximise(model, 1)
-    _fix_glucose_maximise(model, 10)
+    _fix_glucose_maximise(model, 1, 'Max biomass and mAb, glc_uptake = 1')
+    _fix_glucose_maximise(model, 10, 'Max biomass and mAb, glc_uptake = 1')
+
+    # Print components required for biomass and mAb production:
+    _print_reaction(model, 'BIO028')
+    _print_reaction(model, 'BIO029')
 
 
-def _fix_glucose_maximise(model, glc_bound):
+def blocked_reactions(filename):
+    model = _get_model(filename)
+
+    # Perform FVA:
+    _perform_fva(model)
+
+    # Delete formaldehyde transport reactions and maximise:
+    _delete_reactions(model, ['EF0032', 'R00602', 'R00610', 'R00611_mt',
+                              'R01565_mt', 'R07937'])
+    _fix_glucose_maximise(model, 1, 'Max biomass, glc_uptake = 1, ' +
+                          'formaldehyde transport ko')
+
+    # Delete ethanolamine transport reaction and maximise:
+    model = _get_model(filename)
+    _delete_reactions(model, ['EF0035'])
+    _fix_glucose_maximise(model, 1, 'Max biomass, glc_uptake = 1, ' +
+                          'ethanolamine transport ko')
+
+    # Delete NADH transport reaction and maximise:
+    model = _get_model(filename)
+    _delete_reactions(model, ['EF0037'])
+    _fix_glucose_maximise(model, 1, 'Max biomass, glc_uptake = 1, ' +
+                          'NADH transport ko')
+
+    # Fix reversibility of alanine, glutamate transport and maximise:
+    model = _get_model(filename)
+    _set_bounds(model, ['EF0006', 'EF0010'], -1000, 0)
+    _fix_glucose_maximise(model, 1, 'Max biomass, glc_uptake = 1, ' +
+                          'Fix alanine and glutamate transport')
+
+
+def _fix_glucose_maximise(model, glc_bound, title):
     '''Fix glucose uptake flux and maximise.'''
     # Fix glucose uptake to glc_bound:
     glc_transport = model.reactions.EF0001
@@ -29,7 +66,43 @@ def _fix_glucose_maximise(model, glc_bound):
 
     # Optimise:
     model.optimize()
+
+    # Print result:
+    print title
     model.summary()
+    print
+
+
+def _print_reaction(model, reaction_id):
+    rct = model.reactions.get_by_id(reaction_id)
+
+    print '\t'.join([rct.id, rct.name,
+                     rct.build_reaction_string(use_metabolite_names=True)]) + \
+        '\n'
+
+
+def _perform_fva(model):
+    '''Perform FVA.'''
+    flux_var = flux_variability_analysis(model, model.reactions)
+    blocked = flux_var.loc[(abs(flux_var['minimum']) < 1e-6) &
+                           (abs(flux_var['maximum']) < 1e-6)]
+
+    print 'Blocked: %d/%d\n' % (len(blocked), len(flux_var))
+
+    flux_var.to_csv('flux_var.csv')
+    blocked.to_csv('blocked.csv')
+
+
+def _delete_reactions(model, reaction_ids):
+    for reaction_id in reaction_ids:
+        model.reactions.get_by_id(reaction_id).knock_out()
+
+
+def _set_bounds(model, reaction_ids, lower_bound, upper_bound):
+    for reaction_id in reaction_ids:
+        rct = model.reactions.get_by_id(reaction_id)
+        rct.lower_bound = lower_bound
+        rct.upper_bound = upper_bound
 
 
 def _get_model(filename):
@@ -40,7 +113,9 @@ def _get_model(filename):
 def main(args):
     '''main method.'''
     filename = args[0]
+
     prelim_analysis(filename)
+    blocked_reactions(filename)
 
 
 if __name__ == '__main__':
